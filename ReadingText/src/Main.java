cfac2d43da6bab1345539c9c290beb970dc6dc62
ImFrog <22986484+ImFrog@users.noreply.github.com>
2018-12-04 19:20:04 -0800

    ReallyFinished

--- ReadingText/src/Main.java
+++ ReadingText/src/Main.java
@@ -8,13 +8,20 @@ public class Main {
 
     private static void findWord(String word, ArrayList<String> sentenceList) {
         System.out.println("Finding: \"" + word + "\"");
+        int instancesFound = 0;
         for(String sentence:sentenceList) {
             int index = 0;
-            while(sentence.indexOf(" " + word.toLowerCase() + " ", index) != -1) {
-                index = sentence.indexOf(" " + word.toLowerCase() + " ", index) + word.length();
-                System.out.println("Sentence " + sentenceList.indexOf(sentence) + " at " + index);
+            int subStringIndex;
+            while((subStringIndex = sentence.toLowerCase().indexOf(word, index)) != -1) {
+                index = subStringIndex + 1;
+                if((subStringIndex == 0) || " (".contains(sentence.substring(subStringIndex-1,subStringIndex))
+                        || "),.;".contains(sentence.substring(subStringIndex+word.length(),subStringIndex+word.length()+1))) {
+                    System.out.println("Sentence " + sentenceList.indexOf(sentence) + " at " + subStringIndex);
+                    instancesFound++;
+                }
             }
         }
+        if(instancesFound==0) System.out.println("Word was not found");
         System.out.println();
     }
 
@@ -38,6 +45,7 @@ public class Main {
             boolean check = false;
             for(String abbrev: abbreviationList) {
                 check = word.contains(abbrev);
+                if(check) break;
             }
             if (check) {
                 sentence = sentence.concat(word + " ");
@@ -53,11 +61,14 @@ public class Main {
             }
         }
 
+        System.out.println(wordList);
+
         for(String sen:sentenceList) {
             System.out.println(sen + "\r");
         }
 
-        findWord("as",sentenceList);
-        findWord("a", sentenceList);
+        findWord("as", sentenceList);
+        findWord("e.g.", sentenceList);
+        findWord("code", sentenceList);
     }
 }
