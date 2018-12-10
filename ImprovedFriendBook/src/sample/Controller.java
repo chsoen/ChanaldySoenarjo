05a4266b909d64f4a99135728d42d72102afcc4f
ImFrog <22986484+ImFrog@users.noreply.github.com>
2018-12-10 13:40:42 -0800

    ImprovedFriendBook1

--- ImprovedFriendBook/src/sample/Controller.java
+++ ImprovedFriendBook/src/sample/Controller.java
@@ -6,6 +6,9 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Controller {
     public ListView<Friend> listFriends = new ListView<>();
     public TextField txtName;
@@ -14,6 +17,25 @@ public class Controller {
     public Label Name;
     public Label Age;
     public Label Gender;
+    public TextField txtNameFriendList;
+    public ListView listGroups;
+
+    public ArrayList<String> getFiles(){
+        String dir = System.getProperty("user.dir");
+        File mydir = new File(dir);
+
+        File[] files = mydir.listFiles();
+        ArrayList<String> temp = new ArrayList<>();
+
+        for(File f : files){
+            if(f.getName().endsWith(".txt")){
+                temp.add(f.getName().substring(0, f.getName().indexOf(".")));
+                System.out.println(f.getName().substring(0, f.getName().indexOf(".")));
+            }
+        }
+
+        return temp;
+    }
 
     public void createFriend(ActionEvent actionEvent) {
         Friend temp = new Friend(txtName.getText(), Integer.parseInt(txtAge.getText()), txtGender.getText());
@@ -22,7 +44,6 @@ public class Controller {
         txtGender.clear();
         txtAge.clear();
     }
-
 
     public void deleteFriend(ActionEvent actionEvent) {
         Friend temp = listFriends.getSelectionModel().getSelectedItem();
@@ -34,5 +55,26 @@ public class Controller {
         Name.setText(temp.name);
         Age.setText(Integer.toString(temp.age));
         Gender.setText(temp.gender);
+    }
+
+    public void saveListFriends(ActionEvent actionEvent)throws IOException {
+        if(!txtNameFriendList.getText().equals("")) {
+            FileWriter fw = new FileWriter(txtNameFriendList.getText() + ".txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (Friend temp : listFriends.getItems()) {
+                bw.write(temp.name + ";" + temp.age + ";" + temp.gender + ";");
+                bw.newLine();
+            }
+            bw.close();
+            txtNameFriendList.setText("");
+            txtNameFriendList.setPromptText("Friend list saved!");
+        } else {
+            txtNameFriendList.setPromptText("Invalid name");
+        }
+    }
+
+
+    public void loadGroup(ActionEvent actionEvent)throws IOException {
+        FileReader fr = new FileReader
     }
 }
---
+++ ImprovedFriendBook/src/sample/test.java
@@ -0,0 +1,17 @@
+package sample;
+
+import java.io.File;
+
+public class test {
+    public static void main(String[] args) {
+        File[] files;
+        String dir = System.getProperty("user.dir");
+        File mydir = new File(dir);
+        files = mydir.listFiles();
+        for(File f : files){
+            if(f.getName().endsWith(".txt")){
+                System.out.println(f.getName().substring(0, f.getName().indexOf(".")));
+            }
+        }
+    }
+}
