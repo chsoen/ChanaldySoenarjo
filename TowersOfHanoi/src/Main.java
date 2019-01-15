922b05b84e6f6e14bc8a016c29aa9ae8e4bd2020
ImFrog <22986484+ImFrog@users.noreply.github.com>
2019-01-15 13:38:03 -0800

    towers

---
+++ TowersOfHanoi/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    private static int calculateHanoi(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateHanoi(5));
+        System.out.println(calculateHanoi(4));
+    }
+}
