50bfd0a72db1c3b1aa4ece9c6c69ddf65e3b68a6
ImFrog <22986484+ImFrog@users.noreply.github.com>
2019-03-02 09:04:32 -0800

    words

--- Illiad/src/Main.java
+++ Illiad/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -8,16 +9,32 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(new File("illiad.txt"));
 
-        HashSet<Word> words = new HashSet<>();
+        HashSet<String> words = new HashSet<>();
+
+        String[] filters = {"'ve", "'s", "'ll", "'", "\"", ",", ";", ":", "(", ")", ".", "?", "!"};
+        //Problem with words like 'tis, en' 'a, ??? the' ???
+
 
         while (scan.hasNext()) {
-            words.add(new Word(scan.next().toLowerCase()));
+            String word = scan.next();
+
+            if (word.endsWith("-")) {
+                word = word.substring(0,word.length()-1);
+            }
+
+            for (String filter: filters) {
+                word = word.toLowerCase().replace(filter, " ").trim();
+            }
+
+            words.add(word);
         }
 
-        Iterator<Word> wordsIter = words.iterator();
-
-        while (wordsIter.hasNext()) {
-            System.out.println(wordsIter.next());
+        for (String word: words) {
+            if (word.length() < 4) {
+                System.out.print(word + " ");
+            }
         }
+
+        System.out.println(words.size());
     }
 }
