22e4e657da3ba24166d694a5f43f370adb7789f8
ImFrog <22986484+ImFrog@users.noreply.github.com>
2019-05-24 10:00:49 -0700

    Chat

--- MessagingApp/src/UI/Controllers/Chatroom.java
+++ MessagingApp/src/UI/Controllers/Chatroom.java
@@ -1,4 +1,9 @@
 package UI.Controllers;
 
+import javafx.scene.input.InputMethodEvent;
+
 public class Chatroom {
+    public void checkKey(InputMethodEvent inputMethodEvent) {
+        System.out.println("Typing");
+    }
 }
